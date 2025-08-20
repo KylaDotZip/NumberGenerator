34c0ce0144ee0dbef80fec1338ff844ff0a92027
Kyla Abigail Wy <kyla@MacBook-Air-Kyla.local>
2025-08-20 22:20:31 +0700

    please forgive me

---
+++ ilovecoding.py
@@ -0,0 +1,206 @@
+import random
+
+n = random.randint(1, 100)
+
+if n == 1:
+    print("1")
+elif n == 2:
+    print("2")
+elif n == 3:
+    print("3")
+elif n == 4:
+    print("4")
+elif n == 5:
+    print("5")
+elif n == 6:
+    print("6")
+elif n == 7:
+    print("7")
+elif n == 8:
+    print("8")
+elif n == 9:
+    print("9")
+elif n == 10:
+    print("10")
+elif n == 11:
+    print("11")
+elif n == 12:
+    print("12")
+elif n == 13:
+    print("13")
+elif n == 14:
+    print("14")
+elif n == 15:
+    print("15")
+elif n == 16:
+    print("16")
+elif n == 17:
+    print("17")
+elif n == 18:
+    print("18")
+elif n == 19:
+    print("19")
+elif n == 20:
+    print("20")
+elif n == 21:
+    print("21")
+elif n == 22:
+    print("22")
+elif n == 23:
+    print("23")
+elif n == 24:
+    print("24")
+elif n == 25:
+    print("25")
+elif n == 26:
+    print("26")
+elif n == 27:
+    print("27")
+elif n == 28:
+    print("28")
+elif n == 29:
+    print("29")
+elif n == 30:
+    print("30")
+elif n == 31:
+    print("31")
+elif n == 32:
+    print("32")
+elif n == 33:
+    print("33")
+elif n == 34:
+    print("34")
+elif n == 35:
+    print("35")
+elif n == 36:
+    print("36")
+elif n == 37:
+    print("37")
+elif n == 38:
+    print("38")
+elif n == 39:
+    print("39")
+elif n == 40:
+    print("40")
+elif n == 41:
+    print("41")
+elif n == 42:
+    print("42")
+elif n == 43:
+    print("43")
+elif n == 44:
+    print("44")
+elif n == 45:
+    print("45")
+elif n == 46:
+    print("46")
+elif n == 47:
+    print("47")
+elif n == 48:
+    print("48")
+elif n == 49:
+    print("49")
+elif n == 50:
+    print("50")
+elif n == 51:
+    print("51")
+elif n == 52:
+    print("52")
+elif n == 53:
+    print("53")
+elif n == 54:
+    print("54")
+elif n == 55:
+    print("55")
+elif n == 56:
+    print("56")
+elif n == 57:
+    print("57")
+elif n == 58:
+    print("58")
+elif n == 59:
+    print("59")
+elif n == 60:
+    print("60")
+elif n == 61:
+    print("61")
+elif n == 62:
+    print("62")
+elif n == 63:
+    print("63")
+elif n == 64:
+    print("64")
+elif n == 65:
+    print("65")
+elif n == 66:
+    print("66")
+elif n == 67:
+    print("67")
+elif n == 68:
+    print("68")
+elif n == 69:
+    print("69")
+elif n == 70:
+    print("70")
+elif n == 71:
+    print("71")
+elif n == 72:
+    print("72")
+elif n == 73:
+    print("73")
+elif n == 74:
+    print("74")
+elif n == 75:
+    print("75")
+elif n == 76:
+    print("76")
+elif n == 77:
+    print("77")
+elif n == 78:
+    print("78")
+elif n == 79:
+    print("79")
+elif n == 80:
+    print("80")
+elif n == 81:
+    print("81")
+elif n == 82:
+    print("82")
+elif n == 83:
+    print("83")
+elif n == 84:
+    print("84")
+elif n == 85:
+    print("85")
+elif n == 86:
+    print("86")
+elif n == 87:
+    print("87")
+elif n == 88:
+    print("88")
+elif n == 89:
+    print("89")
+elif n == 90:
+    print("90")
+elif n == 91:
+    print("91")
+elif n == 92:
+    print("92")
+elif n == 93:
+    print("93")
+elif n == 94:
+    print("94")
+elif n == 95:
+    print("95")
+elif n == 96:
+    print("96")
+elif n == 97:
+    print("97")
+elif n == 98:
+    print("98")
+elif n == 99:
+    print("99")
+elif n == 100:
+    print("100")
+else:
+    print("I hate myself ts pmo sybau.")
